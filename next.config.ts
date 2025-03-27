6250b638f0d8c3e17a9a68b258c6702117611ac2
omkarbandikatte <omkarbandikatte2602@gmail.com>
2025-03-27 20:20:33 +0530

    Updated code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // No rewrites needed for FastAPI, Vercel handles it
 };
 
